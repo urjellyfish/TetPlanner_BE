89165d1262f813af54cf0599d93fb127dbc04ecb
urjellyfish <annaphan0511@gmail.com>
2026-02-25 09:22:46 +0700

    feat: add project structure

---
+++ src/main/java/com/wecamp/TetPlanner_BE/TetPlannerBeApplication.java
@@ -0,0 +1,13 @@
+package com.wecamp.TetPlanner_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TetPlannerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TetPlannerBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wecamp/TetPlanner_BE/TetPlannerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wecamp.TetPlanner_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TetPlannerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
